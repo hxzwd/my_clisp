f9f5fea82619d25cf0a1c29a14704abdc6bdc0b9
hxzwd <omnomnomrus43@gmail.com>
2015-05-04 14:03:39 -0700

    dobavili fichu

--- test.lisp
+++ test.lisp
@@ -3,3 +3,7 @@
 (setq expression (read))
 (setq result (eval expression))
 (print result)
+(print "enter some list: ")
+(setq expression (read))
+(print "perviy element: ")
+(print (car expression))
